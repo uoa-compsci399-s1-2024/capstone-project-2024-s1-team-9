76dfd945c8499554e94a9200d6b399264557b158
jzho954 <jzho954@aucklanduni.ac.nz>
2024-04-28 20:03:27 +1200

    added footer
    added footer

--- hsrcalculator/src/App.js
+++ hsrcalculator/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CategorySelector from './Components/CategorySelector.jsx'
 import Non_DairyBeverages from './Calculators/Non_DairyBeverages.jsx'
+import Footer from './Components/Footer.js'
 
 
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
     <h1>Health Star Rating Calculator</h1>
     <CategorySelector />
+    <Footer />
   </div>
   )
 }
